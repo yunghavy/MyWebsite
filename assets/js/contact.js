4b177476d97f27ad9946a41b4e5c95b76e82c34b
kimcipher <kimutaij192@gmail.com>
2023-06-30 11:38:39 +0300

    responsive preset page and contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,24 +1,24 @@
 
-$('.menu-toggle').click(function(){
-   $(".nav").toggleClass("mobile-nav");
-   $(this).toggleClass("is-active");
+// Toggle mobile navigation
+$('.menu-toggle').click(function() {
+  $(".nav").toggleClass("mobile-nav");
+  $(this).toggleClass("is-active");
 });
 
-// end of navbar
-
+// Get form elements
 const nameEl = document.querySelector("#name");
 const emailEl = document.querySelector("#email");
 const subjectEl = document.querySelector("#subject");
 const messageEl = document.querySelector("#message");
-
 const form = document.querySelector("#contact-form");
-
+// Validate form fields
 function checkValidations() {
   let letters = /^[a-zA-Z\s]*$/;
   const nameValue = nameEl.value.trim();
   const emailValue = emailEl.value.trim();
   const subjectValue = subjectEl.value.trim();
   const messageValue = messageEl.value.trim();
+
   if (nameValue === "") {
      document.querySelector(".name-error").classList.add("error");
       document.querySelector(".name-error").innerText =
@@ -37,10 +37,23 @@ function checkValidations() {
       document.querySelector(".email-error").innerText =
         "Please fill out this field!";
       } else {
-        // Add email validation logic if needed
         document.querySelector(".email-error").innerText = "";
       }
     }
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Perform form validation
+  checkValidations();
+
+  // If all fields are valid, submit the form
+  if (!document.querySelector('.name-error').classList.contains('error') &&
+      !document.querySelector('.email-error').classList.contains('error')) {
+    handleSubmit(event);
+  }
+});
+   
 
 
 function reset() {
@@ -51,15 +64,10 @@ function reset() {
   document.querySelector(".name-error").innerText = "";
   document.querySelector(".email-error").innerText = "";
 }
- // end of form styles
 
- // validation of contact form
 
-  /* Form submissions */
-  document.addEventListener('DOMContentLoaded', function() {
-      document.getElementById('contact-form').addEventListener('submit', handleSubmit);
-    });
 
+// Handle form submission
     function handleSubmit(event) {
       event.preventDefault(); // Prevent form submission
 
@@ -85,7 +93,7 @@ function reset() {
       };
       xhr.send(new FormData(form));
     }
-
+// Show message in result div
     function showMessage(message, type) {
       var resultDiv = document.getElementById('result');
       var messageElement = document.createElement('div');
@@ -93,3 +101,8 @@ function reset() {
       messageElement.className = type;
       resultDiv.appendChild(messageElement);
     }
+  // Validate email address using regular expression  
+  function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  }
